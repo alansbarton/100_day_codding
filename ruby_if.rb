c069f91022c0e3c08441c6ae863bba671d89e1f5
Alan Barton <abarton201@gmail.com>
2021-07-31 08:54:32 -0400

    ruby practice

--- ruby_if.rb
+++ ruby_if.rb
@@ -19,9 +19,38 @@ end
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
+x = 9
+y = 11
+
+if x < 10 && y < 10
+  p 1
+else
+  p 0
+end
+
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
+x = 9000
+
+if x > 9000
+  p 1
+else
+  p -1
+end
+
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+x = 39
+
+if x < 10
+  p 9
+elsif x < 20
+  p 19
+elsif x < 30
+  p 29
+else
+  p 0
+end
 
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
