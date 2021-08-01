9a3e7fff1f437e9184564e3409c049eea6ebcefa
Alan Barton <abarton201@gmail.com>
2021-08-01 09:54:44 -0400

    ruby practice

--- ruby_if.rb
+++ ruby_if.rb
@@ -49,17 +49,68 @@ elsif x < 20
 elsif x < 30
   p 29
 else
-  p 0
+  p -1
 end
 
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
+x = 9
+y = 6
+
+if x > 10 || y > 10
+  p 100
+else
+  p -100
+end
+
 # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+x = -6
+
+if x < 0
+  p 1776
+else
+  p 1979
+end
 
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
 
+x = 99
+
+if x == 100
+  p 100
+elsif x == 99
+  p 99
+else
+  p 0
+end
+
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
 
-# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+x = 10
+y = 20
+
+if x < 0 && y > 0
+  p 1
+else
+  p 0
+end
+
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80,
+# prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+x = 21
+
+if x > 80
+  p 5
+elsif x > 60
+  p 4
+elsif x > 40
+  p 3
+elsif x > 20
+  p 2
+else
+  p 1
+end
 
 # SOLUTIONS: https://gist.github.com/peterxjang/aee70f966f0f725609eee89b06e8a6c0
