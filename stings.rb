2e5263a4828007682599002ac8c2a3e2b8c1ee61
Alan Barton <abarton201@gmail.com>
2021-08-05 15:32:46 -0400

    practice

--- stings.rb
+++ stings.rb
@@ -33,12 +33,48 @@ p "#{x} #{y} #{z}"
 
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
+puts "Enter a name"
+
+name = gets.chomp
+
+if name != "Santa"
+  puts "You're not Santa"
+end
+
 # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+title = "Santa marie"
+author = "me"
+
+p title + " " + author
 
 # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
 
+p "#{title} #{author}"
+
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
+puts "enter a password"
+
+password = gets.chomp
+
+if password == "Joshua"
+  p "Shall we play a game?"
+else
+  p "Access denied"
+end
+
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+p "enter a city"
+city1 = gets.chomp
+
+p "enter a city"
+city2 = gets.chomp
+
+p "enter a city"
+city3 = gets.chomp
+
+p city1 + city2 + city3
 
 # SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
